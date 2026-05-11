a3d7bf570c69a6d120fddeb7848386d03d958c1d
Kulwant Singh <ikulwant@amazon.com>
2026-05-11 14:57:34 -0700

    Rename to non-test file so CodeQL analyzes it

--- agent/taskresource/fsxwindowsfileserver/vuln_windows.go
+++ agent/taskresource/fsxwindowsfileserver/vuln_windows.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Vulnerable: unsanitized user input flows directly into exec.Command
-func handleRequest_windows(w http.ResponseWriter, r *http.Request) {
+func handleMountRequest_windows(w http.ResponseWriter, r *http.Request) {
 	username := r.URL.Query().Get("username")
 	cmd := fmt.Sprintf("$(New-Object System.Management.Automation.PSCredential('%s', $(ConvertTo-SecureString 'pass' -AsPlainText -Force)))", username)
 	out, _ := exec.Command("powershell.exe", cmd).CombinedOutput()
